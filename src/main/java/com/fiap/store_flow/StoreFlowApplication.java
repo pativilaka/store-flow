4ed7bdb96bf0f1712e9ca86f797c40dfbfff31f2
pativilaka <p.vilaka@gmail.com>
2024-10-26 07:48:46 -0300

    Projeto adicionado.

---
+++ src/main/java/com/fiap/store_flow/StoreFlowApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.store_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/store_flow/StoreFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.store_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
