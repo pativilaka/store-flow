15a4d588b0655442243de04ad17dfe886bef2d23
pativilaka <p.vilaka@gmail.com>
2024-11-03 21:41:44 -0300

    Add User e config h2

--- src/main/java/com/fiap/store_flow/StoreFlowApplication.java
+++ src/main/java/com/fiap/store_flow/StoreFlowApplication.java
@@ -2,9 +2,11 @@ package com.fiap.store_flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.service.annotation.GetExchange;
 
 @SpringBootApplication
 public class StoreFlowApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreFlowApplication.class, args);
